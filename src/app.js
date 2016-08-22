8c3244c31843759d3c5ede71d12773d81da9f7d7
Eric Xu <exu@fitbit.com>
2016-08-21 17:03:33 -0700

    Nodejs local client server + electron-based dashboard for demo and debugging.

--- src/app.js
+++ src/app.js
@@ -1,24 +1,117 @@
-// Here is the starting point for your application code.
+// Here is the starting point for your application code (not the electron main).
 // All stuff below is just to show you how it works. You can delete all of it.
 
 // Use new ES6 modules syntax for everything.
-import os from 'os'; // native node.js module
-import { remote } from 'electron'; // native electron module
+import { remote, ipcRenderer } from 'electron'; // native electron module
 import jetpack from 'fs-jetpack'; // module loaded from npm
-import { greet } from './hello_world/hello_world'; // code authored by you in this project
 import env from './env';
 
 console.log('Loaded environment variables:', env);
 
 var app = remote.app;
+
 var appDir = jetpack.cwd(app.getAppPath());
 
 // Holy crap! This is browser window with HTML and stuff, but I can read
 // here files like it is node.js! Welcome to Electron world :)
 console.log('The author of this app is:', appDir.read('package.json', 'json').author);
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('greet').innerHTML = greet();
-    document.getElementById('platform-info').innerHTML = os.platform();
-    document.getElementById('env-name').innerHTML = env.name;
+function fetchClassExtension(className, callback) {
+    var http = require('http');
+    const url = "http://api.ai.codes/jvm/usage/" + className;
+    http.get(url, (response) => {
+        var body = '';
+        response.on('data', function(d) {
+            body += d;
+        });
+        response.on('end', function() {
+            var parsed = JSON.parse(body);
+            callback(parsed);
+        });
+    });
+}
+
+var iceModel = new Map();
+
+function updateICEDisplay(error, iceId, intention, className_context, extension) {
+    "use strict";
+    // TODO(exu): Objectify iceModel.
+    if (iceModel.get('id') == iceId) {
+        if (! iceModel.get('context').has(className_context)) {
+            iceModel.get('context').add(className_context);
+            iceModel.get('extension').add(extension);
+        }
+    } else {    // construct a new ice Model
+        iceModel.clear();
+        iceModel.set('id', iceId);
+        iceModel.set('intention', intention)
+        iceModel.set('context', new Set());
+        iceModel.get('context').add(className_context);
+        iceModel.set('extension', new Set());
+        iceModel.get('extension').add(extension);
+    }
+    iceModelToDisplay(iceModel);
+}
+
+function iceModelToDisplay(iceModel) {
+    var intentionNode = document.getElementById("intention");
+    var contextNode = document.getElementById("context");
+    var extensionNode = document.getElementById("extension");
+    // Clean slade
+    while (intentionNode.firstChild) {
+        intentionNode.removeChild(intentionNode.firstChild);
+    }
+    while (contextNode.firstChild) {
+        contextNode.removeChild(contextNode.firstChild);
+    }
+    while (extensionNode.firstChild) {
+        extensionNode.removeChild(extensionNode.firstChild);
+    }
+
+    intentionNode.appendChild(document.createTextNode(iceModel.get('intention')));
+    for (let item of iceModel.get('context')) {
+        contextNode.appendChild(document.createTextNode(item + "; "));
+    }
+    for (let item of iceModel.get('extension')) {
+        extensionNode.appendChild(
+            document.createElement("PRE").appendChild(
+                document.createElement("CODE").appendChild(
+                    document.createTextNode(JSON.stringify(item, null, 2)))));
+    }
+}
+
+var plotGoogleCharts = function(usage) {
+    "use strict";
+    var json_usage = JSON.parse(usage);
+    var data = new google.visualization.DataTable();
+
+    data.addColumn('string', 'Method');
+    data.addColumn('number', 'Frequency');
+
+    for (var name in json_usage) {
+        data.addRow([name, json_usage[name]]);
+    }
+    var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
+    var options = {
+        width: 500,
+        height: 800,
+        bar: {groupWidth: "95%"},
+        legend: 'none',
+        vAxis: {
+            textStyle: {
+                'fontName': 'Source Code Pro',
+            }
+        }
+    };
+    chart.draw(data, options);
+};
+
+ipcRenderer.on('ice-display', updateICEDisplay);
+
+ipcRenderer.on('ice-lookup', (event, iceId, intention, className) => {
+    fetchClassExtension(className, extension => {
+        "use strict";
+        ipcRenderer.send('ice-cache', className, extension);
+        updateICEDisplay(null, iceId, intention, className, extension);
+    });
 });
--- src/background.js
+++ src/background.js
@@ -3,7 +3,7 @@
 // It doesn't have any windows which you can see on screen, but we can open
 // window from here.
 
-import { app, Menu } from 'electron';
+import { app, Menu, ipcMain } from 'electron';
 import { devMenuTemplate } from './menu/dev_menu_template';
 import { editMenuTemplate } from './menu/edit_menu_template';
 import createWindow from './helpers/window';
@@ -11,8 +11,6 @@ import createWindow from './helpers/window';
 // Special module holding environment variables which you declared
 // in config/env_xxx.json file.
 import env from './env';
-
-var mainWindow;
 
 var setApplicationMenu = function () {
     var menus = [editMenuTemplate];
@@ -30,21 +28,75 @@ if (env.name !== 'production') {
     app.setPath('userData', userDataPath + ' (' + env.name + ')');
 }
 
+var cache = new Map();
+
 app.on('ready', function () {
     setApplicationMenu();
 
     var mainWindow = createWindow('main', {
-        width: 1000,
-        height: 600
+        width: 500,
+        height: 800
     });
 
     mainWindow.loadURL('file://' + __dirname + '/app.html');
+    let content = mainWindow.webContents;
 
     if (env.name === 'development') {
         mainWindow.openDevTools();
     }
+
+    // ==== ai.codes code begin ========
+    // TODO(exu): move these to a separate js file.
+    var http = require('http');
+
+    function handleRequest(request, response){
+        if (request.url == '/favicon.ico') {
+            response.end('');
+            return;
+        }
+
+        var res = request.url.substring(1).split('/');
+        if (res.length != 2) {
+            response.end("Not a valid URL");
+            return;
+        }
+
+        var iceId = res[0];
+        var className = res[1];
+
+        var result = {
+            '.expiresIn': 1,    // default cache TTL for cold lookup: 1s
+        };
+
+        console.log("check if cache has \'" + className + "\'");
+        if (cache.has(className)) {
+            result = cache.get(className);
+            content.send("ice-display", iceId, "method name auto-complete;", className, result);
+        } else {
+            content.send("ice-lookup", iceId, "method name auto-complete;", className);
+        }
+        response.writeHead(200, {"Content-Type": "application/json"});
+        response.end(JSON.stringify(result));   // has to be a string or buffer...
+    }
+
+    const PORT = 26337;
+    var server = http.createServer(handleRequest);
+    server.listen(PORT, function(){
+        //Callback triggered when server is successfully listening. Hurray!
+        console.log("Server listening on: http://localhost:%s", PORT);
+    });
+    // ====== ai.codes code ends =========
+
 });
 
 app.on('window-all-closed', function () {
     app.quit();
+});
+
+// Store the class -> extension (JSON object) mapping to cache.
+ipcMain.on('ice-cache', (event, className, extension) => {
+    "use strict";
+    console.log('In Main process, storing to cache \'' + className + '\' -> ' + JSON.stringify(extension, null, 2));
+    console.log('JS cache size is  ' + cache.size);
+    cache.set(className, extension);
 });
